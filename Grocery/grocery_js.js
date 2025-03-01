5f8a8a9905eabeb01275399d4854b0e128cd89c4
Greeshma-Mohan <greeshmamohan350@gmail.com>
2025-03-01 11:45:55 -0500

    grocery

---
+++ Grocery/grocery_js.js
@@ -0,0 +1,10 @@
+let first_grocery_amount;
+let second_grocery_amount;
+let third_grocery_amount;
+function total_amount(){
+    first_grocery_amount = document.getElementById('grocery_amount_1').value;
+    second_grocery_amount = document.getElementById('grocery_amount_2').value;
+    third_grocery_amount = document.getElementById('grocery_amount_3').value;
+    let total_amount = first_grocery_amount + second_grocery_amount + third_grocery_amount;
+}
+document.getElementById('grocery_amount').innerHTML(total_amount)
