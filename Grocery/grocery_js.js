fca3ff4c43f72f59c22e5bfdd505076131007e02
Greeshma-Mohan <greeshmamohan350@gmail.com>
2025-03-01 12:09:08 -0500

    grocery

--- Grocery/grocery_js.js
+++ Grocery/grocery_js.js
@@ -2,9 +2,9 @@ let first_grocery_amount;
 let second_grocery_amount;
 let third_grocery_amount;
 function calculate_grocery(){
-    first_grocery_amount = document.getElementById('grocery_amount_1').value;
-    second_grocery_amount = document.getElementById('grocery_amount_2').value;
-    third_grocery_amount = document.getElementById('grocery_amount_3').value;
+    first_grocery_amount = parseFloat(document.getElementById('grocery_amount_1').value);
+    second_grocery_amount = parseFloat(document.getElementById('grocery_amount_2').value);
+    third_grocery_amount = parseFloat(document.getElementById('grocery_amount_3').value);
     let total_amount = first_grocery_amount + second_grocery_amount + third_grocery_amount;
     document.getElementById('grocery_amount').innerText = `The total amount is : ${total_amount}`;
 }
