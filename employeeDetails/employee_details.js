9e2c15af93b1a8e24481aae779a2fe2ce80fc452
Greeshma Mohan <greeshmamohan350@gmail.com>
2025-03-02 12:30:09 -0500

    Employee Details

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,7 +1,7 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization : "Javascript" },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization : "PHP" },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization : "Python" },
     //... More employee records can be added here
   ];
   function displayEmployees(){
@@ -26,6 +26,15 @@ function findEmployeeById(employeeId) {
     }
     else{
       document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
+
+    }
+}
+
+function findBySpecialization(specitzn){
+    const spEmp = employees.find(employee => employee.specialization == specitzn);
+    if(spEmp){
+        document.getElementById('employeesDetails').innerHTML =`<p>${spEmp.id}: ${spEmp.name} - ${spEmp.department} - ${spEmp.specialization}</p>`;
+    }else{
 
     }
 }
