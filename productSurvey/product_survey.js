daa0d9a470ddfc1e6b27a21ae027ad1533f933a9
Greeshma Mohan <greeshmamohan350@gmail.com>
2025-03-02 07:04:25 -0500

    product survay

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -6,6 +6,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const userExperince = document.getElementById('userExperince').value;
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
     document.getElementById('userEmail').innerHTML = email;
@@ -13,8 +14,9 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperince').innerHTML = userExperince;
     document.getElementById('userInfo').style.display = 'block';
-alert('Thank you for your valuable feedback');
+    alert('Thank you for your valuable feedback');
 }
 
 const submitButton=document.getElementById('submitBtn');
